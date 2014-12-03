1b84fea0ce4fd176137d50a3061cb294fe09979a
sd263 <simondate@yahoo.co.uk>
2014-12-03 16:49:19 +0000

    added game timer
    Problem with ticker not ending properly trying to fix

--- ebf.js
+++ ebf.js
@@ -1,15 +1,21 @@
 var stage;
 var stageBottom = 800;
 var stageRight = 1200;
-var square;
-var keys = {}; 
-var time;
-var heroJump = false; // would ideally be an atribute of hero
+var keys = {};
+var lives = 5; 
+var scoreMultiplier = 5;
 
- function init() {
-           var stage = new createjs.Stage("demoCanvas");  // variable causes whole code to run in 1 function fix when possible
-		   
-		   var circle = new createjs.Shape();
+
+	function init() {
+			stage = new createjs.Stage("demoCanvas");  // variable causes whole code to run in 1 function fix when possible
+			homeScreen();
+	}
+
+	function homeScreen(){
+			
+			stage.removeAllChildren();
+			
+			var circle = new createjs.Shape();
 			circle.graphics.beginFill("red").drawCircle(0, 0, 50);
 			circle.x = 250;
 			circle.y = 250;
@@ -28,77 +34,141 @@ var heroJump = false; // would ideally be an atribute of hero
 				start.addEventListener("click", startGame);
 
 			stage.update();
+		}
 
-	function startGame(event) {
-		stage.removeAllChildren();	// removes all menu items
-		stage.update();
-		renderLevel(0);
-//	    nextLevel(3,0);
-	}
-	
-	
-	function renderLevel(level){
+	function startGame() {
 		
-		if(level == 0){  // mario level
+		var roundNumber = 1;
+			stage.removeAllChildren();	// removes all menu items
+			stage.update();
+			var level = 0 // code for random number
+			renderLevel(level);
+			
+			
+	//	while(lives > 0){
+			
+			
+			//  var result = renderLevel(level);  // preferred code
+			//	roundResult(result,roundNumber);
+/* 			if(result == false)
+				lives--;
+			else
+				scoreMultiplier--; */
+				
+			// lives--; // game crashes without
+//		}
+	}
 
+
+	function renderLevel(level) {
+		if(level == 0){  // mario level
+			var counter = 0;
+			var levelTime = 2;  // 5s to complete level
+			var roundTime = scoreMultiplier * levelTime;
+			var heroJump = false; // would ideally be an attribute of hero
+		
 			var floor = new createjs.Shape();
 			floor.graphics.beginFill("#ff0000").drawRect(0, stageBottom-100, stageRight, 100);
 			stage.addChild(floor);
 			
 			var hero = new createjs.Shape();
-			hero.graphics.beginFill("#AAA").drawRect(100, stageBottom-180, 30, 80);
+			hero.graphics.beginFill("#AAA").drawRect(50, stageBottom-180, 30, 80);
 			stage.addChild(hero);		
 			
-			time=new Date();
-			var title = new createjs.Text(time, "10px Arial", "#ff7700"); 
-			title.x = 200;
-			title.y = 40;
-			stage.addChild(title);
-			
-			stage.update(event);
+			var timeLeft = new createjs.Text(roundTime, "10px Arial", "#ff7700"); 
+			timeLeft.x = 200;
+			timeLeft.y = 40;
+			stage.addChild(timeLeft);
+
 			
 			createjs.Ticker.on("tick", tick);
 			createjs.Ticker.setFPS(60);
 			
-			    this.document.onkeydown = keydown;
-				this.document.onkeyup = keyup;
+			this.document.onkeydown = keydown;
+			this.document.onkeyup = keyup;
 				
-				
-		function keydown(event) {
-			keys[event.keyCode] = true;
-		}
-	
-		function keyup(event) {
-			delete keys[event.keyCode];
-		}
 
-	function tick(event) {
-
-		if(heroJump == true){
-		console.log("jumping");
-			hero.y -= 8;
-			if(hero.y <=  -200)
-			heroJump = false;
-		}
-	
-		if(hero.y < 0 && heroJump == false){
-			console.log("going down");
-			hero.y += 8;
-		}
-	
-		if (keys[37]) hero.x -= 10;
-		if (keys[38] && hero.y == floor.y) heroJump = true;
-		if (keys[39]) hero.x += 10;
-		if (keys[40]) init();
-		stage.update(event);
-	}
+			function tick(event) {
+				counter =  parseInt(this.getTime()/1000);
+				timeLeft.text = roundTime - counter;
 				
-		}
 		
-	}
+				if(roundTime - counter <= 0){
+					event.remove();
+					roundResult(false,1);
+					// return false;  // leaves the level and reports result
+				}
+					
+				else if(hero.x >= stageRight-200){
+					event.remove();
+					roundResult(true,1); 
+						
+					// return true;  // leaves the level and reports result
+				} 
+					
+		
+				if(heroJump == true){
+					hero.y -= 8; // go up
+					if(hero.y <= -200)
+						heroJump = false;  // begin falling
+				}
+	
+				if(hero.y < 0 && heroJump == false){
+					hero.y += 8;
+				}
+	
+				if (keys[37]) hero.x -= 25;
+				if (keys[38] && hero.y == floor.y) heroJump = true;
+				if (keys[39]) hero.x += 25;
+				
+				stage.update(event);
 			
+			}
+				
+		}
+	}
 
-     }			
-/* 	function  nextLevel(var lives, var round) {
-	alert("hi");
-	} */
+	function roundResult(result,roundNumber){
+		stage.removeAllChildren();
+		
+		
+			var outcome = new createjs.Text( "", "80px Arial", "#ff7700"); 
+			outcome.x = 200;
+			outcome.y = 40;
+			
+			var currentRound = new createjs.Text(roundNumber, "80px Arial" , "#ff7700"); 
+				currentRound.x = 200;
+				currentRound.y = 340;
+				
+			stage.addChild(outcome);
+			stage.addChild(currentRound);	
+			stage.update();
+			
+		if(result == true){
+		outcome.text = "you win";
+		}	
+		else if(result == false)
+			outcome.text = "you loose";
+			
+		sleep(1000);	
+		renderLevel(0);	
+			
+	}
+
+
+	function keydown(event) {
+			keys[event.keyCode] = true;
+	}
+	
+	function keyup(event) {
+			delete keys[event.keyCode];
+	}
+	
+	function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
